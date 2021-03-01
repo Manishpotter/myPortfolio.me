13e5a0763ff408ae6af9bc6001573af16f6a0241
Manishpotter <76894219+Manishpotter@users.noreply.github.com>
2021-03-01 21:18:45 +0530

    Delete annoy.js

--- project-3/annoy.js
+++
@@ -1,6 +0,0 @@
-var answer = prompt("Are we there yet ?");
-while( answer.indexOf("yes") === -1 && answer.indexOf("YES") === -1)
-{
-    var ask = prompt("Are we there yet ?");
-}
-alert("YAY , we made it !")
